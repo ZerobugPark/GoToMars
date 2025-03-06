197a9765a56a72e7331cf0b6fa324e5ba93f2dd3
young <zerobugpark@gmail.com>
2025-03-06 16:08:20 +0900

    [화면추가] - 탭바별 화면 추가

---
+++ GoToMars/Exchange/ExchangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExchangeViewController.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/6/25.
+//
+
+import UIKit
+
+class ExchangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
