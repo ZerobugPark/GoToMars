c2c0dd3595a7492f571b3caafe4b9fab3ed5f9d2
young <zerobugpark@gmail.com>
2025-03-06 15:19:04 +0900

    Initial Commit

---
+++ GoToMars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
