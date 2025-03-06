1374b8b3501e6b3a4b6f2f60d090051588e6e01a
young <zerobugpark@gmail.com>
2025-03-07 00:34:54 +0900

    [로직구현]
    - 네트워크 매니저 기본 로직 구현

--- GoToMars/NetworkManger/NetworkManager.swift
+++ GoToMars/NetworkManger/NetworkManager.swift
@@ -6,3 +6,134 @@
 //
 
 import Foundation
+
+import Alamofire
+import RxSwift
+
+enum APIError: Int, Error {
+    
+    case test = 400
+    case test2 = 401
+    case test3 = 404 // baseURL Error
+}
+
+enum APIRequest {
+    
+    
+    case upbit
+    
+    
+    var baseURL: String {
+        switch self {
+        case .upbit:
+            "https://api.upbit.com/v1/ticker/all"
+        }
+    }
+    
+    
+    var endPoint: URL {
+        switch self {
+        case .upbit:
+            return URL(string: baseURL)!
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var parameter: Parameters? {
+        switch self {
+            
+        case .upbit:
+            let parameters = ["quote_currencies": "KRW"]
+            return parameters
+        }
+    }
+    
+    
+    
+}
+
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() { }
+    
+    
+    func callRequest<T: Decodable>(api: APIRequest, type: T.Type) -> Single<Result<T, APIError>> {
+        
+        
+        return Single<Result<T, APIError>>.create { value in
+            
+            
+            let api = APIRequest.upbit
+            AF.request(api.endPoint, method: api.method, parameters: api.parameter, encoding: URLEncoding(destination: .queryString)).validate(statusCode: 200...299).responseDecodable(of: [UpBitAPI].self) {
+                response in
+                
+                switch response.result {
+                case .success(let data):
+                    
+                    if let data = data as? T {
+                        value(.success(.success(data)))
+                    } else {
+                        value(.success(.failure(APIError.test)))
+                    }
+                    
+                    
+                case .failure(let error):
+                    dump(error)
+                    
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case APIError.test.rawValue:
+                            print("tests")
+                        case APIError.test2.rawValue:
+                            print("t2ests")
+                        case APIError.test3.rawValue:
+                            print("ddsada")
+                        default:
+                            print("t2ests22")
+                        }
+                    }
+                    
+                    //print(response.response?.statusCode)
+                }
+                
+                
+            }
+            
+            return Disposables.create()
+        }
+        
+       
+    }
+    
+    
+    
+//    func callRequest() {
+//        
+//        let api = APIRequest.upbit
+//        AF.request(api.endPoint, method: api.method, parameters: api.parameter, encoding: URLEncoding(destination: .queryString)).validate(statusCode: 200...299).responseDecodable(of: [UpBitAPI].self) {
+//            response in
+//            
+//            switch response.result {
+//            case .success(let value):
+//                dump(value)
+//            case .failure(let error):
+//                dump(error)
+//                
+//                print(response.response?.statusCode)
+//            }
+//            
+//            
+//        }
+//        
+//    }
+    
+    
+    
+    
+}
