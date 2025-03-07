012f49a5ab621c3f29b573fbb39d6a6ecb7ffd5f
young <zerobugpark@gmail.com>
2025-03-08 02:23:22 +0900

    [UI수정] - 폰트 수정

--- GoToMars/ReuseView/NavigationTitleView.swift
+++ GoToMars/ReuseView/NavigationTitleView.swift
@@ -9,8 +9,8 @@ import UIKit
 
 final class NavigationTitleView: BaseView {
     
-    let titleLabel = CustomLabel(bold: true, fontSize: 20, color: .projectNavy)
-    
+    let titleLabel = CustomLabel(bold: true, fontSize: 16, color: .projectNavy)
+        
     override func configureHierarchy() {
         
         self.addSubview(titleLabel)
@@ -22,12 +22,12 @@ final class NavigationTitleView: BaseView {
         titleLabel.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.leading.equalToSuperview()
-            make.height.equalTo(20)
         }
         
         
     }
-
+    
+    
     deinit {
         print("NavigationTitleView Deinit")
     }
