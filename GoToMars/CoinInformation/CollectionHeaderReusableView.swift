ebf7686784edc71f2b5bdd4f925537d8d7ded288
young <zerobugpark@gmail.com>
2025-03-07 20:45:34 +0900

    [파일 추가]
    - 헤더관리를 위한 컬렉션 뷰 헤더 추가

---
+++ GoToMars/CoinInformation/CollectionHeaderReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionHeaderReusableView.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/7/25.
+//
+
+import UIKit
+
+class CollectionHeaderReusableView: UICollectionReusableView {
+        
+}
