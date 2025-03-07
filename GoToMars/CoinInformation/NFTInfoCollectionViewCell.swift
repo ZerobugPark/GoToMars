f442855e0282995919e5c31f10f1e4115ea7e8f7
young <zerobugpark@gmail.com>
2025-03-07 19:16:55 +0900

    [파일추가]
    NFT Info CollectionView Cell

---
+++ GoToMars/CoinInformation/NFTInfoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NFTInfoCollectionViewCell.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/7/25.
+//
+
+import UIKit
+
+class NFTInfoCollectionViewCell: UICollectionViewCell {
+    
+}
