b2b78594978d365d813d5544650f4e309894bb9e
young <zerobugpark@gmail.com>
2025-03-08 22:22:14 +0900

    [파일추가[
    - CoinDetailVeiw
    - CoinDetailViewController

---
+++ GoToMars/CoinDetailView/CoinDetailView.swift
@@ -0,0 +1,12 @@
+//
+//  CoinDetailView.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/8/25.
+//
+
+import UIKit
+
+class CoinDetailView: BaseView {
+
+}
---
+++ GoToMars/CoinDetailView/CoinDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CoinDetailViewController.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/8/25.
+//
+
+import UIKit
+
+class CoinDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
