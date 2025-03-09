8b3384d4859865198cd7ba9fa3a5b10de5bf3eab
young <zerobugpark@gmail.com>
2025-03-09 19:25:01 +0900

    [파일추가]
    CoinViewModel 추가

---
+++ GoToMars/CoinDetailView/CoinDetailViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CoinDetailViewModel.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/9/25.
+//
+
+import Foundation
+
+import RxCocoa
+import RxSwift
+
+
+
+final class CoinDetailViewModel: BaseViewModel {
+    
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    init() {
+        
+    }
+    
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+    
+}
