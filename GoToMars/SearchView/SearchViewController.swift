c1f918215723bce08605a8bb9d22a1f4e25598c0
young <zerobugpark@gmail.com>
2025-03-10 14:57:40 +0900

    [파일추가]
    SearchViewContoller 추가

---
+++ GoToMars/SearchView/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/10/25.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+        
+        view.backgroundColor = .lightGray
+    }
+    
+
+
+}
