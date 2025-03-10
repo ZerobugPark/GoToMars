4dcac8e2a9f6aba643e2645bbeadcaea06a5278b
young <zerobugpark@gmail.com>
2025-03-10 15:19:25 +0900

    [파일추가]
    - SearchView

---
+++ GoToMars/SearchView/SearchView.swift
@@ -0,0 +1,50 @@
+//
+//  SearchView.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/10/25.
+//
+
+import UIKit
+
+final class SearchView: BaseView {
+
+   
+    let container = UIView()
+    
+    
+    
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    override func configureHierarchy() {
+        
+
+
+    }
+    
+    override func configureLayout() {
+   
+        
+
+        
+        
+
+    }
+    
+    override func configureView() {
+       
+
+    }
+    
+    
+    
+    
+    
+    
+
+}
