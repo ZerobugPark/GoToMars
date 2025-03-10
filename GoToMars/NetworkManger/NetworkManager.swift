cff36202351c44f48ed65e63b79463414ea605c0
young <zerobugpark@gmail.com>
2025-03-11 02:30:40 +0900

    에어 코드 추가

--- GoToMars/NetworkManger/NetworkManager.swift
+++ GoToMars/NetworkManger/NetworkManager.swift
@@ -12,9 +12,17 @@ import RxSwift
 
 enum APIError: Int, Error {
     
-    case test = 400
-    case test2 = 401
-    case test3 = 404 // baseURL Error
+    case badRequest = 400   // 잘못된 정보 요청
+    case unauthorized = 401 // 허가되지 않은 서명
+    case forbidden = 403 // 서버에서 차단되어 요청을 승인할 수 없음
+    case baseURLError = 404 // 잘못된 요청 URL
+    case tooManyRequests = 429 // 요청 횟수 제한 (잠시 후 다시 시도)
+    case internalServerError = 500 // 서버에서 방생한 오류
+    case serviceUnavailable   = 503 // 서비스 이용 불가
+    case accessDenied = 1020 // CDN 방화벽 규칙 위반
+    case apiKeyMissing = 10002 // 잘못된 API키
+    case noconnection = 999 // 인터넷 연결 끊김
+    case unknown = 99999 // 임의의 에러 값
 }
 
 enum APIRequest {
