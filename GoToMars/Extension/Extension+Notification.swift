0d579d8116ee36d68c70842b83227701a0ece480
young <zerobugpark@gmail.com>
2025-03-11 16:23:10 +0900

    [파일추가] Notification 추가

---
+++ GoToMars/Extension/Extension+Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Extension+Notification.swift
+//  GoToMars
+//
+//  Created by youngkyun park on 3/11/25.
+//
+
+import Foundation
+
+
+extension NSNotification.Name {
+    static let isLiked = NSNotification.Name("isLiked")
+}
