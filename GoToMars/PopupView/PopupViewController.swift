de4cd941439bcc5efee7e7ce22888639481100cc
young <zerobugpark@gmail.com>
2025-03-11 19:55:44 +0900

    네트워크 연결 확인에 대한 타임 인터벌 조정

--- GoToMars/PopupView/PopupViewController.swift
+++ GoToMars/PopupView/PopupViewController.swift
@@ -32,26 +32,26 @@ final class PopupViewController: UIViewController {
     private func bind() {
         
         // 다시시도하기 버튼을 누르지 않더라도, 네트워크 연결이 되면 이전 화면으로 돌아가기
-        Observable<Int>.interval(.seconds(2), scheduler: MainScheduler.instance).bind(with: self) { owner, _ in
+        Observable<Int>.interval(.seconds(10), scheduler: MainScheduler.instance).bind(with: self) { owner, _ in
             
             if NetworkMonitor.shared.isConnected {
                 
-                owner.view.makeToast("네트워크에 연결되었습니다.", duration: 0.5)
+                owner.view.makeToast("네트워크에 연결되었습니다.")
                 owner.test.accept(())
                 owner.dismiss(animated: true)
                 
             } else {
-                owner.view.makeToast("네트워크 연결이 원활하지 않습니다.", duration: 1.0)
+                owner.view.makeToast("네트워크 연결이 원활하지 않습니다.")
             }
             
         }.disposed(by: disposeBag)
         
         popupView.restartButton.rx.tap.bind(with: self) { owner, _ in
             
-            owner.view.makeToast("네트워크 연결을 다시 시도합니다.", duration: 0.5)
+            owner.view.makeToast("네트워크 연결을 다시 시도합니다.")
             
             if NetworkMonitor.shared.isConnected {
-                owner.view.makeToast("네트워크에 연결되었습니다.", duration: 0.5)
+                owner.view.makeToast("네트워크에 연결되었습니다.")
                 owner.test.accept(())
                 owner.dismiss(animated: true)
             }
